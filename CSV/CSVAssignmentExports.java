a420d2b1c7a17d858850cd55abab6939f665d29d
SrivyshnaviK <srivyshnavi.koduri@zemosolabs.com>
2021-06-16 20:48:34 +0530

    CourseEra CSV Assignments

---
+++ CSV/CSVAssignmentExports.java
@@ -0,0 +1,55 @@
+package com.Vyshnavi.CSV.exports;
+
+import edu.duke.FileResource;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+public class CSVAssignment {
+    public void tester(){
+        FileResource fileResource=new FileResource();
+        CSVParser csvParser=fileResource.getCSVParser();
+        //System.out.println(countryInfo(csvParser,"Nauru"));
+        //csvParser=fileResource.getCSVParser();
+        //listExportersTwoProducts(csvParser,"cotton","flowers");
+        //csvParser= fileResource.getCSVParser();
+        //System.out.println(numberOfExporters(csvParser,"cocoa"));
+        //csvParser=fileResource.getCSVParser();
+        bigExporters(csvParser,"$999,999,999,999");
+    }
+    public String countryInfo(CSVParser csvParser, String countryName){
+        for (CSVRecord csvRecord:csvParser) {
+            String country=csvRecord.get("Country");
+            if(country.equals(countryName))
+                return csvRecord.get("Country")+":"+csvRecord.get("Exports")+":"+csvRecord.get("Value (dollars)");
+        }
+        return "NOT FOUND";
+    }
+    public void listExportersTwoProducts(CSVParser csvParser,String exportItem1,String exportItem2){
+        for (CSVRecord csvRecord:csvParser) {
+            String exports=csvRecord.get("Exports");
+            if(exports.contains(exportItem1) && exports.contains(exportItem2))
+                System.out.println(csvRecord.get("Country"));
+        }
+    }
+    public int numberOfExporters(CSVParser csvParser,String exportItem){
+        int count=0;
+        for (CSVRecord csvRecord:csvParser) {
+            String exports=csvRecord.get("Exports");
+            if(exports.contains(exportItem))
+                count++;
+        }
+        return count;
+    }
+    public void bigExporters(CSVParser csvParser,String amount){
+        for (CSVRecord csvRecord:csvParser) {
+            String value=csvRecord.get("Value (dollars)");
+            if(value.length()>amount.length())
+                System.out.println(csvRecord.get("Country")+" "+csvRecord.get("Value (dollars)"));
+        }
+    }
+    public static void main(String[] args) {
+        CSVAssignment csvAssignment=new CSVAssignment();
+        csvAssignment.tester();
+
+    }
+}
